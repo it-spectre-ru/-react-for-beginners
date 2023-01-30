ed86141b17c115f234f186c86f98f3cc5c246e5c
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-01-30 15:46:19 +0000

    input profile informations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
-
+import * as React from 'react'
 import './App.css'
 import Header from './Header'
-import Content from './Content'
+import Employees from './Employees'
 import Footer from './Footer'
 
 export default function App() {
   return (
-    <main>
+    <div>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
-    </main>
+    </div>
   )
 }
