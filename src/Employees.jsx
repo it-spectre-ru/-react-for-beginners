57475ce078460a8b80fae34560921de1304f50a3
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-01-30 16:07:02 +0000

    styling card

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -91,18 +91,21 @@ const Employees = () => {
 
   return (
     <main className='container'>
-      <div className='row'>
+      <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
-          {
-            employees.map((employee) => (
-              <div id={employee.id} className='card'>
-                <img src={femaleProfile} className='card-img-top' />
-                <div className='card-body'>
-                  <h5 className='card-title'>Full name: {employee.fullName}</h5>
+          <div className='card-collection'>
+            {
+              employees.map((employee) => (
+                <div id={employee.id} className='card m-2' style={{cursor:'pointer'}}>
+                  <img src={femaleProfile} className='card-img-top' />
+                  <div className='card-body'>
+                    <h5 className='card-title'>Full name: {employee.fullName}</h5>
+                    <p className='card-text'><b>Designation:</b> {employee.designation}</p>
+                  </div>
                 </div>
-              </div>
-            ))
-          }
+              ))
+            }
+          </div>  
         </div>
       </div>
     </main>
