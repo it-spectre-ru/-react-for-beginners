a38a304fe09557e16d6f01c7190e37632dc06788
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-01-31 09:29:09 +0000

    input card gender

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -97,7 +97,11 @@ const Employees = () => {
             {
               employees.map((employee) => (
                 <div id={employee.id} className='card m-2' style={{cursor:'pointer'}}>
-                  <img src={femaleProfile} className='card-img-top' />
+                  
+                  {(employee.gender === 'male') ? 
+                  <img src={maleProfile} className='card-img-top' /> :
+                  <img src={femaleProfile} className='card-img-top' />}
+                  
                   <div className='card-body'>
                     <h5 className='card-title'>Full name: {employee.fullName}</h5>
                     <p className='card-text'><b>Designation:</b> {employee.designation}</p>
