60927cf1960adf7843f4eb99f65c98359ed69344
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-01-31 10:34:05 +0000

    fix bug

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -95,8 +95,10 @@ const Employees = () => {
     setTeam(event.target.value)
   }
 
-  function handleEmployeeCardClick (event) {
-    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTaeget.id)?(employee.teamName === selectedTeam)?{...employee, teamName:''}:{...employee, teamName: selectedTeam}:employee);
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+      ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' }
+        : { ...employee, teamName: selectedTeam } : employee);
 
     setEmployees(transformedEmployees);
   }
@@ -107,25 +109,25 @@ const Employees = () => {
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
-            <option value='TeamA'>Team A</option>          
-            <option value='TeamB'>Team B</option>          
-            <option value='TeamC'>Team C</option>          
-            <option value='TeamD'>Team D</option>          
+            <option value='TeamA'>Team A</option>
+            <option value='TeamB'>Team B</option>
+            <option value='TeamC'>Team C</option>
+            <option value='TeamD'>Team D</option>
           </select>
-        </div> 
-      </div> 
-      
+        </div>
+      </div>
+
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <div className='card-collection'>
             {
               employees.map((employee) => (
-                <div id={employee.id} className={(employee.teamName === selectedTeam?'card m-2 standout':'card m-2')} style={{cursor:'pointer'}} onClick={handleEmployeeCardClick}>
-                  
-                  {(employee.gender === 'male') ? 
-                  <img src={maleProfile} className='card-img-top' /> :
-                  <img src={femaleProfile} className='card-img-top' />}
-                  
+                <div id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: 'pointer' }} onClick={handleEmployeeCardClick}>
+
+                  {(employee.gender === 'male') ?
+                    <img src={maleProfile} className='card-img-top' /> :
+                    <img src={femaleProfile} className='card-img-top' />}
+
                   <div className='card-body'>
                     <h5 className='card-title'>Full name: {employee.fullName}</h5>
                     <p className='card-text'><b>Designation:</b> {employee.designation}</p>
@@ -133,7 +135,7 @@ const Employees = () => {
                 </div>
               ))
             }
-          </div>  
+          </div>
         </div>
       </div>
     </main>
