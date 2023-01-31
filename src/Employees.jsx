af80ce8d59534e0d4b558b803b00139af1820612
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-01-31 10:14:12 +0000

    selected Team handler

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,6 +4,8 @@ import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
+  const [selectedTeam, setTeam] = useState('TeamB');
+
   const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
@@ -89,8 +91,25 @@ const Employees = () => {
     teamName: "TeamD"
   }]);
 
+  function handleTeamSelectionChange(event) {
+    console.log(event.target.value)
+    setTeam(event.target.value)
+  }
+
   return (
     <main className='container'>
+
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-8'>
+          <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+            <option value='TeamA'>Team A</option>          
+            <option value='TeamB'>Team B</option>          
+            <option value='TeamC'>Team C</option>          
+            <option value='TeamD'>Team D</option>          
+          </select>
+        </div> 
+      </div> 
+      
       <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <div className='card-collection'>
